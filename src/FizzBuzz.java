2a1896d126e4c656a74edaa9efc974f783a7ccff
JqrqmyChqn <chanjeremy2015@gmail.com>
2024-09-11 12:52:09 -0400

    Added Main and Updated FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,7 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main() {
         int i = 1;
         while (i != 101){
             // Find out which numbers divide i.
@@ -11,7 +11,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
